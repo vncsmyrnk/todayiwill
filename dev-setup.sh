369ff0ff986adcaca78e855b54aad5750bf7468c
Vinicius <vmayrink12@gmail.com>
2024-06-24 18:52:25 +0000

    Adding script to setup dev environment

---
+++ dev-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update -qq
+apt-get install -qq -y curl wget
+
+# Set up git
+bash <(curl -s https://raw.githubusercontent.com/vncsmyrnk/git-config/main/install-apt.sh) "$GIT_USERNAME" "$GIT_EMAIL" --config-only
+
+# Set up neovim
+bash <(curl -s https://raw.githubusercontent.com/vncsmyrnk/vim-config/main/install-apt.sh)
+
+# Remove log files
+rm .*log
