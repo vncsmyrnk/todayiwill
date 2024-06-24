d0beb97ecd4eae5f1452eba171dedd10aac2f0fa
Vinicius <vmayrink12@gmail.com>
2024-06-24 12:58:17 +0000

    Adding fn for parsing a file

--- todayiwill/src/appointment/list.rs
+++ todayiwill/src/appointment/list.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
 use crate::appointment::AppointmentTime;
 
 use super::Appointment;
@@ -20,12 +25,13 @@ pub fn get_appointments() -> Vec<Appointment> {
     ]
 }
 
-// fn read_file_content(file_path: &str) -> Result<Vec<String>, io::Error> {
-//     let mut file_content = String::new();
-//     File::open(file_path)?.read_to_string(&mut file_content)?;
-//     let appointments: Vec<String> = file_content.lines().map(parse_file_line).collect();
-//     Ok(appointments)
-// }
+fn read_file_content(file_path: &str) -> Result<Vec<Appointment>, io::Error> {
+    let mut file_content = String::new();
+    File::open(file_path)?.read_to_string(&mut file_content)?;
+    let appointments: Vec<Option<Appointment>> =
+        file_content.lines().map(parse_file_line).collect();
+    Ok(appointments.into_iter().flatten().collect())
+}
 
 fn parse_file_line(line: &str) -> Option<Appointment> {
     let time: String = line.chars().take(5).collect();
@@ -43,10 +49,15 @@ fn parse_file_line(line: &str) -> Option<Appointment> {
 
 #[cfg(test)]
 mod tests {
-    use crate::appointment::{list::parse_file_line, Appointment, AppointmentTime};
+    use std::{fs::File, io::Write};
+
+    use crate::appointment::{
+        list::{parse_file_line, read_file_content},
+        Appointment, AppointmentTime,
+    };
 
     #[test]
-    fn test_parse_ok() {
+    fn parse_wellformed_line() {
         let result = parse_file_line("16:03 This is an example").unwrap();
         assert_eq!(
             result,
@@ -61,8 +72,31 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_fail() {
+    fn pase_malformed_line_time_fail() {
         let result = parse_file_line("10:0 This is an incorrect example");
         assert!(result.is_none());
+    }
+
+    #[test]
+    fn parse_file_contents() {
+        let test_file_path = "/tmp/test_file.txt";
+        let mut file = File::create(test_file_path).expect("Failed to create test file");
+        file.write_all(b"22:00 Go to night shift\n12:45 Visit grandma\n212 Nonsense")
+            .expect("Failed to write to test file");
+        let result = read_file_content(test_file_path).expect("Failed to read test file");
+        assert_eq!(
+            result,
+            vec![
+                Appointment::new("Go to night shift".to_string(), AppointmentTime::new(22, 0)),
+                Appointment::new("Visit grandma".to_string(), AppointmentTime::new(12, 45)),
+            ]
+        );
+        std::fs::remove_file(test_file_path).expect("Failed to delete test file");
+    }
+
+    #[test]
+    fn parse_non_existent_file_should_err() {
+        let result = read_file_content("Non-existent.txt");
+        assert!(result.is_err());
     }
 }
