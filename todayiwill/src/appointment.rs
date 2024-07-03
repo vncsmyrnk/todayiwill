3ce757f484ccfd396ff5ce70cc122d4993ab7674
Vinicius <vmayrink12@gmail.com>
2024-07-03 22:01:05 +0000

    Adding `Sub` implementation on `AppointmentTime` and method `is_past_from_now` on both

--- todayiwill/src/appointment.rs
+++ todayiwill/src/appointment.rs
@@ -1,5 +1,9 @@
 use core::fmt;
-use std::{ops::Add, path::PathBuf, str};
+use std::{
+    ops::{Add, Sub},
+    path::PathBuf,
+    str,
+};
 
 use chrono::{Local, NaiveDate};
 
@@ -57,6 +61,10 @@ impl AppointmentTime {
         }
     }
 
+    pub fn is_past_from_now(&self) -> bool {
+        self <= &Self::now()
+    }
+
     pub fn from(time: &str) -> Result<Self, String> {
         let (hour, minutes) = match helper::parse_time(time) {
             Some((hour, minutes)) => (hour, minutes),
@@ -70,6 +78,13 @@ impl AppointmentTime {
         Self {
             hour: 23,
             minutes: 59,
+        }
+    }
+
+    pub fn min_value() -> Self {
+        Self {
+            hour: 0,
+            minutes: 0,
         }
     }
 }
@@ -86,6 +101,23 @@ impl Add<i32> for AppointmentTime {
         Self {
             hour: hours_updated,
             minutes: minutes_updated % 60,
+        }
+    }
+}
+
+impl Sub<i32> for AppointmentTime {
+    type Output = AppointmentTime;
+
+    fn sub(self, rhs: i32) -> Self::Output {
+        let minutes_updated = self.minutes - rhs;
+        let hours_updated =
+            self.hour + (minutes_updated / 60) - if minutes_updated.is_negative() { 1 } else { 0 };
+        if hours_updated < 0 {
+            return AppointmentTime::min_value();
+        }
+        Self {
+            hour: hours_updated,
+            minutes: minutes_updated.abs() % 60,
         }
     }
 }
@@ -120,6 +152,10 @@ impl Appointment {
         let appointment_time = AppointmentTime::from(&time)?;
         let description = appointment.chars().skip(6).collect();
         Ok(Appointment::new(description, appointment_time))
+    }
+
+    pub fn is_past_from_now(&self) -> bool {
+        self.time.is_past_from_now()
     }
 
     pub fn to_string_display(&self) -> String {
@@ -237,6 +273,12 @@ mod tests {
     }
 
     #[test]
+    fn min_value() {
+        let result = AppointmentTime::min_value();
+        assert_eq!(result, AppointmentTime::new(0, 0).unwrap());
+    }
+
+    #[test]
     fn add_i32_to_appointment_time() {
         let result = AppointmentTime::new(10, 30).unwrap() + 10;
         assert_eq!(result, AppointmentTime::new(10, 40).unwrap());
@@ -258,6 +300,30 @@ mod tests {
     fn add_i32_to_appointment_time_upper_limit_edge_case() {
         let result = AppointmentTime::new(23, 55).unwrap() + 4;
         assert_eq!(result, AppointmentTime::max_value());
+    }
+
+    #[test]
+    fn sub_i32_to_appointment_time() {
+        let result = AppointmentTime::new(10, 30).unwrap() - 20;
+        assert_eq!(result, AppointmentTime::new(10, 10).unwrap());
+    }
+
+    #[test]
+    fn sub_i32_to_appointment_time_edge_case() {
+        let result = AppointmentTime::new(3, 10).unwrap() - 100;
+        assert_eq!(result, AppointmentTime::new(1, 30).unwrap());
+    }
+
+    #[test]
+    fn sub_i32_to_appointment_time_lower_limit() {
+        let result = AppointmentTime::new(0, 5).unwrap() - 20;
+        assert_eq!(result, AppointmentTime::min_value());
+    }
+
+    #[test]
+    fn sub_i32_to_appointment_time_lower_limit_edge_case() {
+        let result = AppointmentTime::new(0, 5).unwrap() - 5;
+        assert_eq!(result, AppointmentTime::min_value());
     }
 
     #[test]
@@ -333,5 +399,35 @@ mod tests {
             AppointmentTime::new(10, 40).unwrap(),
         );
         assert_eq!("[10:40] Go to the dentist", appointment.to_string_display());
+    }
+
+    #[test]
+    fn appointment_time_should_be_passed() {
+        let future_appointment_time = AppointmentTime::now() + 5;
+        assert!(!future_appointment_time.is_past_from_now())
+    }
+
+    #[test]
+    fn appointment_time_should_not_be_passed() {
+        let future_appointment_time = AppointmentTime::now() - 5;
+        assert!(future_appointment_time.is_past_from_now())
+    }
+
+    #[test]
+    fn appointment_should_be_passed() {
+        let future_appointment = Appointment::new(
+            String::from("Some future appointment"),
+            AppointmentTime::now() + 5,
+        );
+        assert!(!future_appointment.is_past_from_now())
+    }
+
+    #[test]
+    fn appointment_not_should_be_passed() {
+        let future_appointment = Appointment::new(
+            String::from("Some past appointment"),
+            AppointmentTime::now() - 5,
+        );
+        assert!(future_appointment.is_past_from_now())
     }
 }
