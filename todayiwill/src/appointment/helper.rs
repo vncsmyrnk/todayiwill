2c4c3eb5f9be754325ce3c019b93dac2abee83f6
Vinicius <vmayrink12@gmail.com>
2024-07-08 16:57:38 +0000

    Adding doc-tests for `helper` mod

--- todayiwill/src/appointment/helper.rs
+++ todayiwill/src/appointment/helper.rs
@@ -3,12 +3,28 @@ use std::path::PathBuf;
 
 use super::helper;
 
+/// Represent the config needed to save the appointments
 pub struct Config {
     pub appointment_file_path_current_day: Box<PathBuf>,
     pub appointment_file_path_builder: Box<dyn Fn(NaiveDate) -> PathBuf>,
 }
 
 impl Config {
+    /// Returns the standard config with the usable paths according to the data dir of the current
+    /// operating system
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// #[cfg(target_os = "linux")]
+    /// use todayiwill::Config;
+    /// use std::path::PathBuf;
+    /// use chrono::{Local, NaiveDate};
+    ///
+    /// let config = Config::standard();
+    /// assert_eq!(PathBuf::from(format!("/home/{}/.local/share/todayiwill/appointments_01012024.txt", whoami::username())), (config.appointment_file_path_builder)(NaiveDate::from_ymd_opt(2024, 1, 1).unwrap()));
+    /// assert_eq!(PathBuf::from(format!("/home/{}/.local/share/todayiwill/appointments_{}.txt", whoami::username(), Local::now().format("%d%m%Y"))), *config.appointment_file_path_current_day);
+    /// ```
     pub fn standard() -> Self {
         let appointment_path_builder = |date: NaiveDate| {
             dirs::data_dir()
@@ -26,7 +42,14 @@ impl Config {
 }
 
 /// Parses string time (hours and minutes) and returns a tuple with both values
-/// `10:43` -> Option<(10, 43)>
+///
+/// # Example
+///
+/// ```
+/// use todayiwill::helper;
+///
+/// assert_eq!((10, 43), helper::parse_time("10:43").unwrap());
+/// ```
 pub fn parse_time(time: &str) -> Option<(i32, i32)> {
     let (hour_str, minutes_str) = time.split_once(':')?;
     let hour = hour_str.parse().ok()?;
@@ -35,12 +58,29 @@ pub fn parse_time(time: &str) -> Option<(i32, i32)> {
 }
 
 /// Returns a string code for a given date
-/// Option<20/01/2021> -> "20012021"
+///
+/// # Example
+///
+/// ```
+/// use todayiwill::helper;
+/// use chrono::NaiveDate;
+///
+/// assert_eq!("31012022", helper::date_code(NaiveDate::from_ymd_opt(2022, 1, 31).unwrap()));
+/// ```
 pub fn date_code(date: NaiveDate) -> String {
     date.format("%d%m%Y").to_string()
 }
 
 /// Converts a string to a naive date
+///
+/// # Example
+///
+/// ```
+/// use todayiwill::helper;
+/// use chrono::NaiveDate;
+///
+/// assert_eq!(NaiveDate::from_ymd_opt(2020, 11, 20).unwrap(), helper::str_dmy_to_naive_date("20/11/2020").unwrap());
+/// ```
 pub fn str_dmy_to_naive_date(date: &str) -> Result<NaiveDate, ParseError> {
     NaiveDate::parse_from_str(date, "%d/%m/%Y")
 }
